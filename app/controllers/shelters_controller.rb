f8b809b4402a9fa177b0ffed40cb4db93dc21e48
Dani Coleman <danicoleman00@gmail.com>
2020-08-20 18:33:11 -0600

    Add an edit action; add update redirect and save

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -22,5 +22,22 @@ class SheltersController < ApplicationController
     @shelter = Shelter.find(params[:id])
   end
 
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update(shelter_params)
+
+    shelter.save
+
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
+  private
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
 
 end
