884c3ea995068eaa12ed7edad6f2e5f525133ee8
Dani Coleman <danicoleman00@gmail.com>
2020-08-21 17:44:32 -0600

    Test delete end point for shelter

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -27,13 +27,35 @@ RSpec.describe "shelters show page", type: :feature do
     expect(page).to_not have_content("#{shelter2.name}")
   end
 
-  it "see a link to update the shelter" do
+  it "sees a link to update the shelter" do
     shelter1 = Shelter.create(name: "Mooses Palace", address: "123 main st", city: "Denver", state: "CO", zip: "80005")
-    shelter2 = Shelter.create(name: "Moon Souls", address: "555 Lunar way", city: "Arvada", state: "CO", zip: "80012")
 
     visit "/shelters/#{shelter1.id}"
 
     expect(page).to have_link("Update Shelter")
     expect(current_path).to eq("/shelters/#{shelter1.id}")
+  end
+  # As a visitor
+  # When I visit a shelter show page
+  # Then I see a link to delete the shelter
+  # When I click the link "Delete Shelter"
+  # Then a 'DELETE' request is sent to '/shelters/:id',
+  # the shelter is deleted,
+  # and I am redirected to the shelter index page where I no longer see this shelter
+  it "see a link to delete shelter" do
+    shelter1 = Shelter.create(name: "Paws", address: "123 main st", city: "Denver", state: "CO", zip: "80005")
+    shelter2 = Shelter.create(name: "Moon Souls", address: "555 Lunar way", city: "Arvada", state: "CO", zip: "80012")
+
+    visit "/shelters/#{shelter1.id}"
+
+
+
+    expect(page).to have_link("Delete Shelter")
+
+    click_link "Delete Shelter"
+    save_and_open_page
+    expect(current_path).to eq("/shelters")
+    expect(page).to_not have_content("#{shelter1.name}")
+    expect(page).to have_content("#{shelter2.name}")
   end
 end
