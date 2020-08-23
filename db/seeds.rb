6b56e6b3ab9098a9da1f3a18710503f6ac0f48be
Dani Coleman <danicoleman00@gmail.com>
2020-08-23 12:25:19 -0600

    Add pet objects to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #Shelters
-shelter1 = Shelter.create(name: "Mooses Palace", address: "123 main st", city: "Denver", state: "CO", zip: "80005")
+shelter1 = Shelter.create(name: "Mooses Palace", address: "123 Main St", city: "Denver", state: "CO", zip: "80005")
 shelter2 = Shelter.create(name: "Moon Souls", address: "555 Lunar way", city: "Arvada", state: "CO", zip: "80012")
+
+pet1 = shelter1.pets.create!(name: "Ari", approximate_age: 2, sex: "Male", image: "ari.jpeg")
+pet2 = shelter2.pets.create!(name: "Axel", approximate_age: 2, sex: "Male", image: "axel.jpeg")
