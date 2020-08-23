fcf92ee410cd0c6bfb1702f900a647114c5e0b78
Dani Coleman <danicoleman00@gmail.com>
2020-08-23 17:14:43 -0600

    Change image path to a string in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ Shelter.destroy_all
 shelter1 = Shelter.create(name: "Mooses Palace", address: "123 Main St", city: "Denver", state: "CO", zip: "80005")
 shelter2 = Shelter.create(name: "Moon Souls", address: "555 Lunar way", city: "Arvada", state: "CO", zip: "80012")
 
-pet1 = Pet.create(name: "Ari", approximate_age: 2, sex: "Male", image: "ari.jpeg", shelter_id: shelter1.id)
-pet2 = Pet.create(name: "Axel", approximate_age: 2, sex: "Male", image: "axel.jpeg", shelter_id: shelter2.id)
+pet1 = Pet.create(name: "Ari", approximate_age: 2, sex: "Male", image: "image", shelter_id: shelter1.id)
+pet2 = Pet.create(name: "Axel", approximate_age: 2, sex: "Male", image: "image", shelter_id: shelter2.id)
