ffdacbce11d042574bf5873de9909c07fb14a0ab
Dani Coleman <danicoleman00@gmail.com>
2020-08-23 18:11:12 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ Shelter.destroy_all
 shelter1 = Shelter.create(name: "Mooses Palace", address: "123 Main St", city: "Denver", state: "CO", zip: "80005")
 shelter2 = Shelter.create(name: "Moon Souls", address: "555 Lunar way", city: "Arvada", state: "CO", zip: "80012")
 
-pet1 = Pet.create(name: "Ari", approximate_age: 2, sex: "Male", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D_-ssNsbP7wA&psig=AOvVaw2auKW-5L8Xb6xZQNcxROVe&ust=1598312346629000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiNjs-_susCFQAAAAAdAAAAABAF", shelter_id: shelter1.id)
-pet2 = Pet.create(name: "Axel", approximate_age: 2, sex: "Male", image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpets.webmd.com%2Fdogs%2Ffeatures%2Fdog-cool-summer&psig=AOvVaw0v-H0ylJHtEr8BDCFN9W8u&ust=1598312421626000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCi8fK_susCFQAAAAAdAAAAABAE", shelter_id: shelter2.id)
+pet1 = Pet.create(name: "Ari", approximate_age: 2, sex: "Male", image: "https://i.pinimg.com/736x/92/f4/5c/92f45cc2ae3a4c1aa6835bb2733ad3ea.jpg", shelter_id: shelter1.id)
+pet2 = Pet.create(name: "Axel", approximate_age: 2, sex: "Male", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Walter_dog.jpg/480px-Walter_dog.jpg", shelter_id: shelter2.id)
